26047c5f8c3ff09b53ca89e11468b4c1ae826523
Sebastian <sebastianjorc@gmail.com>
2023-04-23 00:06:30 -0400

    v-0.1

---
+++ script.js
@@ -0,0 +1,27 @@
+/*// Accede al botón "Ordena Ahora"
+const orderButton = document.querySelector('.cta');
+
+// Agrega un evento al hacer clic en el botón
+orderButton.addEventListener('click', function(event) {
+  // Previene el comportamiento predeterminado del botón
+  event.preventDefault();
+
+  // Coloca aquí tu código para procesar la orden
+});
+
+// Accede al formulario de contacto
+const contactForm = document.querySelector('form');
+
+// Agrega un evento al enviar el formulario
+contactForm.addEventListener('submit', function(event) {
+  // Previene el comportamiento predeterminado del formulario
+  event.preventDefault();
+
+  // Accede a los campos del formulario
+  const nameInput = document.querySelector('#nombre');
+  const emailInput = document.querySelector('#email');
+  const messageInput = document.querySelector('#mensaje');
+
+  // Coloca aquí tu código para enviar el formulario
+});
+*/
